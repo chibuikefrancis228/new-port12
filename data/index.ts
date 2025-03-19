4a225f5e0dfa9a24ba985ae9b2443c0bdf96a394
chibuikefrancis228 <chibuikefrancis228@gmail.com>
2025-03-19 14:26:28 -0700

    h

--- data/index.ts
+++ data/index.ts
@@ -95,7 +95,7 @@ export const projects = [
     link: "/ui.aiimg.com",
   },
   {
-    id: 6,
+    id: 4,
     title: "3D portfolio",
     des: "A visually striking, interactive 3D portfolio that showcases creative work in an immersive, dynamic environment. Using cutting-edge 3D technology, this project enables users to explore artwork and designs in a more engaging and lifelike way, offering a unique and memorable experience.",
     img: "/sibl61rkK9.png",
@@ -103,7 +103,7 @@ export const projects = [
     link: "/ui.aiimg.com",
   },
   {
-    id: 4,
+    id: 5,
     title: "Animated  Website",
     des: "is a dynamic web experience that incorporates engaging animations to enhance user interaction and storytelling. The project showcases the seamless integration of motion design into the user interface, creating a visually captivating environment that keeps visitors engaged. By combining creative animation with intuitive navigation, this website pushes the boundaries of traditional web design, offering a unique, immersive experience that captures attention and conveys content in an exciting, interactive way...",
     img: "/chrome_7wUSnfAoS3.png",
@@ -111,7 +111,7 @@ export const projects = [
     link: "/ui.apple.com",
   },
   {
-    id: 5,
+    id: 6,
     title: "Movie App",
     des: "",
     img: "/scrnli_1_3_2024_2-34-36 PM copy.png",
